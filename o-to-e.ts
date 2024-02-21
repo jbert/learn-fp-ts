9237e573883bda69ba146361ec3c394761f43b30
John Berthels <john.berthels@gmail.com>
2024-02-21 10:13:59 +0000

    use A.head as option source

--- o-to-e.ts
+++ o-to-e.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env ts-node
 import * as TE from "fp-ts/TaskEither";
 import * as E from "fp-ts/Either";
-import * as O from "fp-ts/Option";
+import * as A from "fp-ts/Array";
 import { pipe } from "fp-ts/function";
 
-const program = (): TE.TaskEither<string, string> => {
+const program = (): TE.TaskEither<string, number> => {
+    const a: Array<number> = [1, 2, 3, 4];
     return pipe(
-        O.none,
+        a,
+        A.head,
         TE.fromOption(() => "No quote")
     );
 };
